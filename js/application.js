fc6ada92de51ebb4e9a4d5f1613cfaa4dd050575
Richard Li <xpp@me.com>
2015-07-22 15:55:11 +0800

    Average retail gas prices

---
+++ js/application.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+  var Charts = function(){
+    this.graphData = [];
+  };
+
+  Charts.prototype.makeAjaxRequest = function() {
+    $.ajax({
+      context: this,
+      type: 'GET',
+      url: 'https://www.quandl.com/api/v1/datasets/BTS_MM/RETAILGAS.json?auth_token=E6kNzExHjay2DNP8pKvB',
+      success: function(response){
+        var items = response.data;
+        for (i=0; i < items.length; i++) {
+          item = items[i]
+          this.graphData.push({
+            x: new Date(item[0]),
+            y: item[1]
+          })
+        }
+        console.log(this.graphData);
+        this.graph();
+      }
+    });
+  }
+
+  Charts.prototype.graph = function() {
+    var highchartConfig = {
+      title: {
+        text: 'Average retail gas prices'
+      },
+      subtitle: {
+        text: 'Bureau of Transportation Statistics (Multimodal)'
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      series: [
+        {
+          name: 'US',
+          data: this.graphData.reverse()
+        }
+      ]
+    }
+    $('#chart').highcharts(highchartConfig); //From library
+  }
+
+
+  var chart = new Charts;
+  chart.makeAjaxRequest();
+
+
+});
